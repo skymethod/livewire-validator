02da5ae57faa69def368cbfa54e9ac069c4026c4
John Spurlock <john@skymethod.com>
2023-08-19 18:20:11 -0500

    Update to latest std, denoflare deps.

--- validator-worker/common/deps_xml.ts
+++ validator-worker/common/deps_xml.ts
@@ -1,3 +1,3 @@
-export { parseXml, validateXml, findChildElements, findElementRecursive, computeAttributeMap, qnameEq, qnamesInclude } from 'https://raw.githubusercontent.com/skymethod/denoflare/2023747da8e6a225a4940a7aefab63b30f9dad05/common/xml_parser.ts';
-export { encodeXml, decodeXml } from 'https://raw.githubusercontent.com/skymethod/denoflare/2023747da8e6a225a4940a7aefab63b30f9dad05/common/xml_util.ts';
-export type { ExtendedXmlNode, Qname } from 'https://raw.githubusercontent.com/skymethod/denoflare/2023747da8e6a225a4940a7aefab63b30f9dad05/common/xml_parser.ts';
+export { parseXml, validateXml, findChildElements, findElementRecursive, computeAttributeMap, qnameEq, qnamesInclude } from 'https://raw.githubusercontent.com/skymethod/denoflare/3424ca5d5f8aacceae234f7ff0ae31e968260e8a/common/xml_parser.ts';
+export { encodeXml, decodeXml } from 'https://raw.githubusercontent.com/skymethod/denoflare/3424ca5d5f8aacceae234f7ff0ae31e968260e8a/common/xml_util.ts';
+export type { ExtendedXmlNode, Qname } from 'https://raw.githubusercontent.com/skymethod/denoflare/3424ca5d5f8aacceae234f7ff0ae31e968260e8a/common/xml_parser.ts';
--- validator-worker/deps_cli.ts
+++ validator-worker/deps_cli.ts
@@ -1,7 +1,7 @@
-export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.194.0/path/mod.ts';
-export { parse as parseFlags } from 'https://deno.land/std@0.194.0/flags/mod.ts';
-export { ModuleWatcher } from 'https://raw.githubusercontent.com/skymethod/denoflare/2023747da8e6a225a4940a7aefab63b30f9dad05/cli/module_watcher.ts';
-export { bundle } from 'https://raw.githubusercontent.com/skymethod/denoflare/2023747da8e6a225a4940a7aefab63b30f9dad05/cli/bundle.ts';
-export { fileExists } from 'https://raw.githubusercontent.com/skymethod/denoflare/2023747da8e6a225a4940a7aefab63b30f9dad05/cli/fs_util.ts';
-export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/2023747da8e6a225a4940a7aefab63b30f9dad05/common/bytes.ts';
-export { parseJsonc } from 'https://raw.githubusercontent.com/skymethod/denoflare/2023747da8e6a225a4940a7aefab63b30f9dad05/cli/jsonc.ts';
+export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.198.0/path/mod.ts';
+export { parse as parseFlags } from 'https://deno.land/std@0.198.0/flags/mod.ts';
+export { ModuleWatcher } from 'https://raw.githubusercontent.com/skymethod/denoflare/3424ca5d5f8aacceae234f7ff0ae31e968260e8a/cli/module_watcher.ts';
+export { bundle } from 'https://raw.githubusercontent.com/skymethod/denoflare/3424ca5d5f8aacceae234f7ff0ae31e968260e8a/cli/bundle.ts';
+export { fileExists } from 'https://raw.githubusercontent.com/skymethod/denoflare/3424ca5d5f8aacceae234f7ff0ae31e968260e8a/cli/fs_util.ts';
+export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/3424ca5d5f8aacceae234f7ff0ae31e968260e8a/common/bytes.ts';
+export { parseJsonc } from 'https://raw.githubusercontent.com/skymethod/denoflare/3424ca5d5f8aacceae234f7ff0ae31e968260e8a/cli/jsonc.ts';
