d5767b3d03006f3368a3b78367292b71eeac5c1c
John Spurlock <john@skymethod.com>
2024-03-24 13:03:47 -0500

    Fix minipub deps.

--- validator-worker/common/deps_comments.ts
+++ validator-worker/common/deps_comments.ts
@@ -1,4 +1,2 @@
-// export { makeRateLimitedFetcher, makeThreadcap, updateThreadcap, InMemoryCache } from 'https://raw.githubusercontent.com/skymethod/minipub/a000c0dad18e8a6326b95f89488afe7640d2d741/src/threadcap/threadcap.ts';
-export { makeRateLimitedFetcher, makeThreadcap, updateThreadcap, InMemoryCache } from '../../../minipub/src/threadcap/threadcap.ts';
-// export type { Comment, Threadcap, Cache, Instant, Callbacks, Fetcher, Commenter } from 'https://raw.githubusercontent.com/skymethod/minipub/a000c0dad18e8a6326b95f89488afe7640d2d741/src/threadcap/threadcap.ts';
-export type { Comment, Threadcap, Cache, Instant, Callbacks, Fetcher, Commenter } from '../../../minipub/src/threadcap/threadcap.ts';
+export { makeRateLimitedFetcher, makeThreadcap, updateThreadcap, InMemoryCache } from 'https://raw.githubusercontent.com/skymethod/minipub/fb830e2f5f91da8c7f72ec7bbbc7e4eb192336cb/src/threadcap/threadcap.ts';
+export type { Comment, Threadcap, Cache, Instant, Callbacks, Fetcher, Commenter } from 'https://raw.githubusercontent.com/skymethod/minipub/fb830e2f5f91da8c7f72ec7bbbc7e4eb192336cb/src/threadcap/threadcap.ts';
